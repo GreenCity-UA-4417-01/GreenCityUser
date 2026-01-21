9409611bc10843514a575cc29e457c46df309145
AdamT3H <adamogorodnik@gmail.com>
2026-01-21 20:59:51 +0200

    Fixed bug

--- service/src/main/java/greencity/service/EmailServiceImpl.java
+++ service/src/main/java/greencity/service/EmailServiceImpl.java
@@ -246,11 +246,7 @@ public class EmailServiceImpl implements EmailService {
     public void sendHabitNotification(String name, String email) {
         String subject = "Notification about not marked habits";
         String content = "Dear " + name + ", you haven't marked any habit during last 3 days";
-        if (userRepo.findByEmail(email).isPresent()) {
-            sendEmail(email, subject, content);
-        } else {
-            throw new NotFoundException(ErrorMessage.USER_NOT_FOUND_BY_EMAIL + email);
-        }
+        sendEmail(email, subject, content);
     }
 
     @Override
