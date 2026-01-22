551dcc435e8bddc49722d4fc1b9bf18cd63699bf
Vitalii Maletych <124867918+v-maletych@users.noreply.github.com>
2026-01-22 14:03:15 +0200

    Fix #62: Document 404 Not Found response for GET /user/findById

--- core/src/main/java/greencity/controller/UserController.java
+++ core/src/main/java/greencity/controller/UserController.java
@@ -440,9 +440,10 @@ public class UserController {
      */
     @Operation(summary = "Get User by id")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
-        @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
-        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST, content = @Content),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED, content = @Content),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND, content = @Content)
     })
     @GetMapping("/findById")
     public ResponseEntity<UserVO> findById(@RequestParam Long id) {
